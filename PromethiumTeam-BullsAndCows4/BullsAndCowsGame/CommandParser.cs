a40001bbc78ec0f8ccccb618f37d1c5f47d4e4ec
todorpr <tono_pr@abv.bg>
2013-05-15 03:29:46 -0700

    CommandParser.cs Update

--- PromethiumTeam-BullsAndCows4/BullsAndCowsGame/CommandParser.cs
+++ PromethiumTeam-BullsAndCows4/BullsAndCowsGame/CommandParser.cs
@@ -7,7 +7,30 @@ using System.Threading.Tasks;
 namespace BullsAndCowsGame
 {
     //TODO : Refactor code, that manages commands
-    static class CommandParser
+    public static class CommandParser
     {
+        public static PlayerCommand PlayerInputToPlayerCommand(string playerInput)
+        {
+            if (playerInput.ToLower() == "top")
+            {
+                return PlayerCommand.Top;
+            }
+            else if (playerInput.ToLower() == "restart")
+            {
+                return PlayerCommand.Restart;
+            }
+            else if (playerInput.ToLower() == "help")
+            {
+                return PlayerCommand.Help;
+            }
+            else if (playerInput.ToLower() == "exit")
+            {
+                return PlayerCommand.Exit;
+            }
+            else
+            {
+                return PlayerCommand.Other;
+            }
+        }
     }
 }
