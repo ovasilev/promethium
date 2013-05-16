423d83718ac086b703383f2d38b62fd4f06b236c
todorpr <tono_pr@abv.bg>
2013-05-16 11:03:36 -0700

    PlayerCommand.cs adding comments

--- PromethiumTeam-BullsAndCows4/BullsAndCowsGame/Common/PlayerCommand.cs
+++ PromethiumTeam-BullsAndCows4/BullsAndCowsGame/Common/PlayerCommand.cs
@@ -1,8 +1,17 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+// <copyright file="PlayerCommand.cs" company="TelerikAcademy">
+//     All rights reserved © Telerik Academy 2012-2013
+// </copyright>
+//----------------------------------------------------------------------
+
+using System;
 using System.Linq;
 
 namespace BullsAndCowsGame
 {
+    /// <summary>
+    /// Several options that the player can choose during the game
+    /// </summary>
     public enum PlayerCommand
     {
         Top,
