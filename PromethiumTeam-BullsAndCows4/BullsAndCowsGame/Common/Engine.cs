88cd8ab57ddcffe6f6559034fab52a30978d0ce7
todorpr <tono_pr@abv.bg>
2013-05-16 11:42:37 -0700

    Engine.cs adding comments

--- PromethiumTeam-BullsAndCows4/BullsAndCowsGame/Common/Engine.cs
+++ PromethiumTeam-BullsAndCows4/BullsAndCowsGame/Common/Engine.cs
@@ -1,14 +1,23 @@
-﻿using System;
-using System.Linq;
+﻿//-----------------------------------------------------------------------
+// <copyright file="Engine.cs" company="TelerikAcademy">
+//     All rights reserved © Telerik Academy 2012-2013
+// </copyright>
+//----------------------------------------------------------------------
+
+using System;
 using System.Text;
 
 namespace BullsAndCowsGame
 {
-    //TODO : Refactor code, that manages the gameplay from original class "bikove_i_kravi"
+    /// <summary>
+    /// Class Engine stores the main functionality of the game
+    /// </summary>
     public class Engine
     {
-        //private const int NumberLenght = 4;
-
+        /// <summary>
+        /// This is the method that each new game strats with. It calls several
+        /// other helper methods.
+        /// </summary>
         public void Start()
         {
             PlayerCommand enteredCommand = new PlayerCommand();
@@ -37,6 +46,13 @@ namespace BullsAndCowsGame
             Console.WriteLine("Good bye!");
         }
 
+        /// <summary>
+        /// Method handles entered by the player command <paramref name="enteredCommand"/>, 
+        /// calling other methods each responsible for different interactions.
+        /// </summary>
+        /// <param name="enteredCommand">Options that the game offers to a player</param>
+        /// <param name="playerInput">Command that player enters</param>
+        /// <param name="player">The user that's playing at the moment</param>
         private void ExecuteCommand(PlayerCommand enteredCommand, string playerInput, Player player)
         {
             if (enteredCommand == PlayerCommand.Top)
@@ -76,6 +92,14 @@ namespace BullsAndCowsGame
             }
         }
 
+        /// <summary>
+        /// The method checkes if the player's input <paramref name="playerInput"/>
+        /// is valid. It checks if the input is empty string, if the input lenght is
+        /// correct and if the input contains digits only.
+        /// </summary>
+        /// <param name="playerInput">Player's input</param>
+        /// <returns>Returns <see cref="System.Boolean"/> true or false
+        /// according to if the input is valid or not.</returns>
         private bool IsValidInput(string playerInput)
         {
             if (playerInput == String.Empty || playerInput.Length != NumberManager.NUMBER_LENGHT)
