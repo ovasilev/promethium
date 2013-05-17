94d060e6666b11793d1222a928b6e8520d2be9ee
Kaloyan Gospodinov <kaloyan_gospodinov@yahoo.co.uk>
2013-05-17 14:34:01 +0300

    ScoreBoard & Engine Unit Tests

---
+++ PromethiumTeam-BullsAndCows4/BullsAndCowsGameTests/EngineTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BullsAndCowsGameTests
+{
+    [TestClass]
+    public class EngineTests
+    {
+        [TestMethod]
+        public void Engine_StartGame()
+        {
+        }
+    }
+}
---
+++ PromethiumTeam-BullsAndCows4/BullsAndCowsGameTests/ScoreBoardTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using BullsAndCowsGame.GameObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BullsAndCowsGameTests
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class ScoreBoardTests
+    {
+        [TestMethod]
+        public void ScoreBoard_ToString_EmptyScoreBoard()
+        {
+            ScoreBoard<Player> scoreBoard = new ScoreBoard<Player>();
+
+            var actual = scoreBoard.ToString();
+            var expected = "Top scoreboard is empty.";
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
